869e2e70cbc9fbbc931ea5c4d69709e69172052b
Laura Gómez <laura200292@protonmail.com>
2025-02-19 17:38:08 +0100

    base url api

--- src/servicios/http-axios.js
+++ src/servicios/http-axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL:"http://localhost:3000/",
+    baseURL:"/api/",
     headers: {
-        "Content-Type" : "aplication/json"
+        "Content-Type" : "application/json"
     },
 })
 
