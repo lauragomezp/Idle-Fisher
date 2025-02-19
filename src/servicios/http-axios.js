3eff6779a412af35f0b208f430e0fc8b3b3b9ddc
Laura Gómez <laura200292@protonmail.com>
2025-02-19 17:52:52 +0100

    local and vercel

--- src/servicios/http-axios.js
+++ src/servicios/http-axios.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_URL || "/api/";
+const baseURL =
+  process.env.NODE_ENV === "production" // Si es producción (Vercel)
+    ? "/api" // En producción, la URL base es "/api" (esto es lo que se define en Vercel)
+    : "http://localhost:3000"; // En desarrollo (local), usamos el servidor local
 
 const http = axios.create({
   baseURL: baseURL,
   headers: {
     "Content-Type": "application/json"
-  },
+  }
 });
 
 export default http;
