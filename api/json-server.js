745d34ca2310fba5ffe47b908885e37e8cf38345
Laura Gómez <laura200292@protonmail.com>
2025-02-19 17:23:48 +0100

    peces.json moved to api

--- api/json-server.js
+++ api/json-server.js
@@ -2,7 +2,9 @@ import jsonServer from 'json-server';
 import path from 'path';
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join('public', 'data', 'peces.json')); // Ajusta la ruta si es necesario
+
+const router = jsonServer.router(path.join('api', 'data', 'peces.json')); 
+
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
@@ -11,3 +13,4 @@ server.use(router);
 export default (req, res) => {
   server(req, res);
 };
+
